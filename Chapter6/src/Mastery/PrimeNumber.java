9532a084cc1362a4ac28f538046e5e2f22565e40
49351003 <49351003@L815WD220028.edu.cbe.ab.ca>
2026-04-13 13:24:57 -0600

    1:24PM

--- Chapter6/src/Mastery/PrimeNumber.java
+++ Chapter6/src/Mastery/PrimeNumber.java
@@ -3,8 +3,47 @@ package Mastery;
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		import java.util.Scanner;
 
+		public class PrimeNumbersApp {
+
+		    public static boolean isPrime(int number) {
+		        if (number <= 1) {
+		            return false;
+		        }
+
+		        for (int i = 2; i <= number / 2; i++) {
+		            if (number % i == 0) {
+		                return false;
+		            }
+		        }
+
+		        return true;
+		    }
+
+		    public static void main(String[] args) {
+		        Scanner input = new Scanner(System.in);
+
+		        System.out.print("Enter the first number: ");
+		        int num1 = input.nextInt();
+
+		        System.out.print("Enter the second number: ");
+		        int num2 = input.nextInt();
+
+		        int start = Math.min(num1, num2);
+		        int end = Math.max(num1, num2);
+
+		        System.out.println("Prime numbers between " + start + " and " + end + ":");
+
+		        for (int i = start; i <= end; i++) {
+		            if (isPrime(i)) {
+		                System.out.print(i + " ");
+		            }
+		        }
+
+		        input.close();
+		    }
+		}
 	}
 
 }
